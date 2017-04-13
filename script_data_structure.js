03ad0920670bbefe8ce34f5f2ea2d41cfe793fd6
kolambok <kolambok@gmail.com>
2017-04-13 20:06:50 +0300

    Add files via upload

---
+++ script_data_structure.js
@@ -0,0 +1,19 @@
+/*-----task1-----*/
+
+var now = new Date();
+var options = {
+    weekday: 'long';
+}
+console.log("Today is: "+now.toLocaleString('long', options));
+console.log(now.toTimeString());
+
+/*-----task2-----*/
+
+console.log(now);
+
+/*-----task3-----*/
+
+/*-----task4-----*/
+
+/*-----task5-----*/
+
