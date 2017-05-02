cbde910e8ebab3a04e363961a0bf87f9bc71a663
kolambok <kolambok@gmail.com>
2017-05-02 13:49:22 +0300

    Task 2: second solution

--- javascript_closure_scope.js
+++ javascript_closure_scope.js
@@ -1,39 +1,37 @@
 //-----task1-----
 
-function wrapper() {
-
-  var i = 0;
-
-  function consoleRec(array) {
-    console.log(array[i]);
-      i++;
-      if (i < array.length) consoleRec(array);
+function consoleRec(array,i = 0) {
+  if (array[i]) {
+    console.log(array[i]);	
   }
-  consoleRec(['я', 'умею', 'писать', 'рекурсивные', 'функции']);
+
+  if(i < array.length) {
+    consoleRec(array, i + 1);
+  }
 }
 
-wrapper();
+consoleRec(['я', 'умею', 'писать', 'рекурсивные', 'функции']);
+
 
 //-----task2-----
 
-var allNumbers = [1, 2, 4, 5, 6, 7, 8],
-someNumbers = [1, 2, 'привет', 4, 5, 'ololo', 6, 7, 8],
-noNumbers = ['это', 'массив', 'без', 'чисел'];
+var allNumbers = [1, 2, 4, 5, 6, 7, 8];
+var someNumbers = [1, 2, 'привет', 4, 5, 'ololo', 6, 7, 8];
+var noNumbers = ['это', 'массив', 'без', 'чисел'];
 
 function isAllTrue(source,filterFn) {
 
-  for (var i = 0; i < source.length; i++) {
-
-    if (source.every(filterFn)) return true;
-
-	return false;
-
+  if (source.some(filterFn)) {
+    return false;
+  } else {
+    return true;
   }
+
 }
 
 function isNumber(val) {
 
-  return typeof val === 'number';
+  return typeof val !== 'number';
 
 }
 
